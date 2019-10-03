684a44e963e0cc0977b574c518e5228ac2f543ea
Alois Fleuricourt <afleuricourt@gmail.com>
2019-10-03 13:11:21 +0200

    try to fix background heroku pb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   config.serve_static_files = true
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   # config.assets.compile = false
